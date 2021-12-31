7b9cb474b34bfee5da361485a6acd520ed6b9b59
Marcelo Dias <marcelo.dias@cilia.com.br>
2021-12-30 22:28:33 -0300

    Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'marcelo', password: '111111')
+User.create(username: 'danielly', password: 'senha')
+User.create(username: 'juliano', password: 'segredo')
+User.create(username: 'danilo', password: 'secret')
+User.create(username: 'edinho', password: '123456')
+User.create(username: 'thiago', password: 'boneco123')
+User.create(username: 'gabrielly', password: 'password')
