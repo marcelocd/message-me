2c64da61898f68119208e0f21d18549f3002a931
Marcelo Dias <marcelo.dias@cilia.com.br>
2021-12-31 11:51:57 -0300

    Add has_many messages relationship and username case insentitivity to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,12 @@ class User < ApplicationRecord
   MIN_USERNAME_LENGTH = 3
   MAX_USERNAME_LENGTH = 15
 
-  validates :username, uniqueness: true,
+  validates :username, uniqueness: { case_sensitive: false },
                        presence: true,
                        length: { minimum: MIN_USERNAME_LENGTH,
                                  maximum: MAX_USERNAME_LENGTH }
 
+  has_many :messages
+  
   has_secure_password
 end
